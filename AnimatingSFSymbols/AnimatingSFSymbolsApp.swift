9c88a5e48403be8de99bb8c695ba18eed22f27f9
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-23 10:34:54 +0530

    Initial Commit

---
+++ AnimatingSFSymbols/AnimatingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingSFSymbolsApp.swift
+//  AnimatingSFSymbols
+//
+//  Created by manvendu pathak  on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
