fc45b580ac8639b41f4400ca2738209573ec465f
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-23 11:02:00 +0530

    First Commit:- 23 March, 2024

--- AnimatingSFSymbols/ContentView.swift
+++ AnimatingSFSymbols/ContentView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animate1 = false
+    @State private var animate2 = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Image(systemName: "ellipsis.message")
+                .font(.system(size: 100))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.purple, .gray)
+                .symbolEffect(.bounce,options: .repeat(5) ,value: animate1)
+                .onTapGesture {
+                    animate1.toggle()
+                }
+            
+            Image(systemName: animate2 ? "checkmark.circle" : "touchid")
+                        .font(.system(size: 100))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.purple, .gray)
+                        .symbolEffect(.bounce, value: animate2)
+                        .contentTransition(.symbolEffect(.replace))
+                        .onTapGesture {
+                            animate2.toggle()
+                        }
+                    
+                }
         }
-        .padding()
+        
     }
-}
+
 
 #Preview {
     ContentView()
